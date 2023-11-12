3c3fbe3eec28b1e40b29bb03b2b9868fb15d1bae
Lustucru <ludivine.mauget@centrale-marseille.fr>
2023-11-12 21:32:51 +0100

    changement disponibilite from boolean to char + tests view character detail (pbm de update de disponibilite qd changement d'équipement

--- playground/models.py
+++ playground/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Equipement(models.Model):
     id_equip = models.CharField(max_length=100, primary_key=True)
-    disponibilite = models.BooleanField((""), default=True) # True = disponible, False = pas disponible
+    disponibilite = models.CharField(max_length=20)
     photo = models.CharField(max_length=200)
     def __str__(self):
         return self.id_equip
--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import MoveForm
-from .models import Character
+from .models import Character, Equipement
+from django.db import transaction
 
 def character_list(request):
         characters = Character.objects.all()
         return render(request, 'playground/character_list.html', {'characters': characters})
 
+
 def character_detail(request, id_character):
         character = get_object_or_404(Character, id_character=id_character)
-        lieu = character.lieu
-        form=MoveForm()
-        return render(request, 'playground/character_detail.html', {'character': character, 'lieu': lieu, 'form': form})
+        if request.method == "POST":
+                form = MoveForm(request.POST, instance=character)
+                if form.is_valid():
+                        if character.equipement.disponibilite == "libre" :
+                                        ancien_equip = get_object_or_404(Equipement, id_equip=character.equipement.id_equip)
+                                        ancien_equip.disponibilite = "libre"
+                                        ancien_equip.save()
+                                        form.save(commit=False)
+                                        nouveau_equip = get_object_or_404(Equipement, id_equip=character.equipement.id_equip)
+                                        if nouveau_equip.id_equip != "Épée":
+                                                nouveau_equip.disponibilite = "occupé"
+                                        nouveau_equip.save()
+                                        character.equipement = nouveau_equip
+                                        character.save()
+                                        return redirect('character_detail', id_character=character.id_character)
+                        else :
+                                return redirect('character_detail', id_character=character.id_character)
+                else:
+                        form = MoveForm()
+                        return render(request, 'playground/character_detail.html', {'character': character, 'equipement': character.equipement, 'form': form})
+        else:
+                form = MoveForm()
+                return render(request, 'playground/character_detail.html', {'character': character, 'equipement': character.equipement, 'form': form})
