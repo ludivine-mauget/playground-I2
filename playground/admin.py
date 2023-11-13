b7ad8e3f42702de4c5aea70379248f7a83af30f3
Lustucru <ludivine.mauget@centrale-marseille.fr>
2023-11-13 13:25:54 +0100

    add table Lieu in admin

--- playground/admin.py
+++ playground/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Character, Equipement
+from .models import Character, Equipement, Lieu
 
 admin.site.register(Character)
 admin.site.register(Equipement)
+admin.site.register(Lieu)
